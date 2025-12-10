89433300c93bbf1c851f60bd9924c7f5d272676e
spuemer845 <spuemer845@g.educaand.es>
2025-12-10 09:33:45 +0100

    D: Mejorar la función pedirNumero

--- src/pruebaControlVersiones/Factorial.java
+++ src/pruebaControlVersiones/Factorial.java
@@ -14,15 +14,29 @@ public class Factorial {
             System.out.println("ERROR: el número debe ser mayor o igual que 0.");
         } else {
             System.out.println("Has introducido el número: " + num);
-            System.out.println("RESULTADO - En versiones posteriores se calculará el factorial del número " + num);
+            System.out.println("RESULTADO - El factorial del número " + num + " es " + factorialRec(num));
         }
         	
         sc.close();
     }
 
     private static int pedirNumero() {
-        System.out.print("Introduce un número entero mayor o igual que 0: ");
-        return sc.nextInt();
-    }
+        int num;
+        boolean valido;
 
+        do {
+            System.out.print("Introduce un número entero mayor o igual que 0: ");
+            if (sc.hasNextInt()) {
+                num = sc.nextInt();
+                valido = true;
+            } else {
+                System.out.println("Entrada no válida. Introduce un entero.");
+                sc.next();
+                valido = false;
+                num = -1;
+            }
+        } while (!valido);
+
+        return num;
+    }
 }
